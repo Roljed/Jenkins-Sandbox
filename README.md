f975b44d4f766b45d29304e9afc7b56bdbf344c9
Yaad Nahshon <ydnahshon@gmail.com>
2024-12-13 14:17:45 +0200

    feat: add AWS CLI automation for the remote host

--- README.md
+++ README.md
@@ -25,9 +25,10 @@ docker-compose up -d
 This variables defined in .env file in the project root.
 
 ```bash
-# AWS CLI Connection
+# AWS
 AWS_ACCESS_KEY_ID=
 AWS_SECRET_ACCESS_KEY=
+AWS_S3_BUCKET_NAME=
 
 # MySQL
 MYSQL_PASS=
--- jenkins-data/production-1/remote-db-backup.sh
+++ jenkins-data/production-1/remote-db-backup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-BACKUP_DATE=$(date +%H-%M-%S)
+BACKUP_DATE=$(date +%Y-%m-%d_%H-%M-%S)
+BACKUP_FILE_NAME=backup-db-$BACKUP_DATE.sql
 
-mysqldump -u root -p$MYSQL_PASS -h $MYSQL_DB_CONTAINER $MYSQL_DB_NAME > /tmp/backup-db-$BACKUP_DATE.sql
+mysqldump -u root -p$MYSQL_PASS -h $MYSQL_DB_CONTAINER $MYSQL_DB_NAME > /tmp/$BACKUP_FILE_NAME && \
+echo "Uploading the DB backup" && \
+aws s3 cp /tmp/$BACKUP_FILE_NAME s3://$AWS_S3_BUCKET_NAME/$BACKUP_FILE_NAME && \
+echo "Backup file uploaded"
