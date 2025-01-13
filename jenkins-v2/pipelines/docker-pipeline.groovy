5cac36f491678e28c5e916be825ff751dc9ebe5f
Yaad Nahshon <ydnahshon@gmail.com>
2025-01-13 07:58:47 +0200

    feat: add docker pipeline

--- jenkins-v2/pipelines/docker-pipeline.groovy
+++ jenkins-v2/pipelines/docker-pipeline.groovy
@@ -1,1 +1,32 @@
+pipeline {
+    agent any
+    stages {
+        stage('Without Docker') {
+            steps {
+                sh '''
+                    echo "Without Docker"
+                    ls -la
+                    touch container-no.txt
+                '''
+            }
+        }
+        stage('With Docker') {
+            agent {
+                docker {
+                    image 'node:18-alpine'
+                    reuseNode true // for workspace synchronization
+                }
+            }
+            steps {
+                sh '''
+                    echo "With Docker"
+                    npm --version
+                    ls -la
+                    touch container-yes.txt
+                    ls -la
+                '''
 
+            }
+        }
+    }
+}
